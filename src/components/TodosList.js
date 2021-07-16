7f3143b6135e38044ffc6fe58e092ecb19ebf583
Joe MacDonald <joe.macd26@gmail.com>
2021-07-15 20:46:44 -0700

    second commit

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -2,22 +2,20 @@ import React from 'react'
 import TodoContainer from './TodoContainer'
 import TodoItem from './TodoItem'
 
-class TodosList extends React.Component {
-    render() {
-        return (
-            <ul>
-                {this.props.todos.map(todo => (
-                    <TodoItem 
-                        key={todo.id} 
-                        todo={todo}
-                        handleChangeProps={this.props.handleChangeProps}
-                        deleteTodoProps={this.props.deleteTodoProps}
-                        setUpdate={this.props.setUpdate}
-                    />
-                ))}
-            </ul>
-        );
-    }
+const TodosList = props => {
+    return (
+        <ul>
+            {props.todo.map (todo => (
+                <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    handleChangeProps={props.handleChangeProps}
+                    deleteTodoProps={props.deleteTodoProps}
+                    setUpdate={props.setUpdate}
+                />
+            ))}
+        </ul>
+    )
 }
 
 export default TodosList
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import ReactDOM from "react-dom"
 import React from "react"
 //component files
-import TodoContainer from './functionBased/TodoContainer'
+import TodoContainer from './components/TodoContainer'
 
 //stylesheet
-import './functionBased/App.css'
+import './components/App.css'
 
 ReactDOM.render(
     <React.StrictMode>
